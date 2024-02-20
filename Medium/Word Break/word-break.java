49d469c0d074005279e574a61f3c6a234c750bc6
Noel Mascarenhas <132780449+NoelMascarenhas@users.noreply.github.com>
2024-02-20 15:37:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Word Break/word-break.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+public class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n;
+                    n = sc.nextInt();
+                    ArrayList<String> dictionary = new ArrayList<String>();
+                    for(int i = 0;i<n;i++)
+                        {
+                            String p = sc.next();
+                            dictionary.add(p);
+                        }
+                    String s = sc.next();
+                    Solution obj = new Solution();  
+                    System.out.println(obj.wordBreak(n, s, dictionary));  
+                    
+                }
+        }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static int wordBreak(int n, String s, ArrayList<String> dictionary )
+    {
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true; // Empty string is always present in the dictionary
+        
+        // Convert dictionary to a HashSet for faster lookup
+        HashSet<String> wordSet = new HashSet<>(dictionary);
+        
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[len] ? 1 : 0;
+    }
+}
