485364a55a7764cebfc9badbe4b6c7529dbaf886
Noel Mascarenhas <132780449+NoelMascarenhas@users.noreply.github.com>
2024-02-24 18:14:08 +0530

    Updated Solution - GfG to GitHub

--- Easy/Maximum Sum Problem/maximum-sum-problem.java
+++ Easy/Maximum Sum Problem/maximum-sum-problem.java
@@ -22,12 +22,17 @@ class GfG
 
 //User function Template for Java
 
-class Solution
-{
+class Solution{
     public int maxSum(int n) {
-        if(n==1||n==0){
+        if(n==0 || n==1){
             return n;
         }
-        return Math.max(maxSum(n/2)+maxSum(n/3)+maxSum(n/4),n);
+        int[] dp = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i] = Math.max(dp[i/2]+dp[i/3]+dp[i/4],i);
+        }
+        return dp[n];
     } 
 }
